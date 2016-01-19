d3dca70727e3b6e4ae21b550280ace28062683be
Cheuk Yin Ng <cheukyin699@yahoo.com>
2016-01-18 18:58:31 -0800

    Added portfolio, updated head and classes to contain bootstrap stuff.

---
+++ js/portfolio.js
@@ -0,0 +1,27 @@
+var heightRatio = 0.7;
+
+$(document).ready(function() {
+    var wh = $(window).height();
+
+    // Make the image bigger
+    $('.full-image').css({height: wh});
+    // Center the section in the image
+    $('.full-image').find('.portfolio-head').css({
+        position: 'relative',
+        top: wh * heightRatio * 0.5
+    });
+    // Each section in the portfolio should be a full-screen
+    $('.project').css({'min-height': wh + "px"});
+    $('.project').children().css({'min-height': wh + "px"});
+
+    // fancyBox
+    $('.fancybox').fancybox();
+
+    // Smooth scrolling on click
+    $('a').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+    });
+});
